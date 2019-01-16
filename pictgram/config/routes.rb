4ceefbcdf6b794bae995694677b6262053078394
”mori_winindows” <ihrnogqb@yahoo.co.jp>
2019-01-16 14:57:58 +0900

    windows_0116

--- pictgram/config/routes.rb
+++ pictgram/config/routes.rb
@@ -1,4 +1,5 @@
 # Rails.application.routes.draw do
+  get 'sessions/new'
 #   get 'users/new'
 #   get 'pages/index'
 #   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -7,14 +8,20 @@
 # end
 
 # Rails.application.routes.draw do
+  get 'sessions/new'
 #   get 'users/new'
 #   root 'pages#index'
 #   get 'pages/help'
 # end
 
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'pages#index'
   get 'pages/help'
 
   resources :users
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
