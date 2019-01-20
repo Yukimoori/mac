dc69318cad44fbc48c55378e335b779f6738c995
”mori_mac” <ihrnogqb@yahoo.co.jp>
2019-01-20 21:40:11 +0900

    mac_0120

--- pictgram/config/routes.rb
+++ pictgram/config/routes.rb
@@ -1,19 +1,3 @@
-# Rails.application.routes.draw do
-  get 'sessions/new'
-#   get 'users/new'
-#   get 'pages/index'
-#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-#   root 'pages#index'
-#
-# end
-
-# Rails.application.routes.draw do
-  get 'sessions/new'
-#   get 'users/new'
-#   root 'pages#index'
-#   get 'pages/help'
-# end
-
 Rails.application.routes.draw do
   get 'sessions/new'
   root 'pages#index'
